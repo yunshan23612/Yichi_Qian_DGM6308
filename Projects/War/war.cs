81f12f716d336483c865ca2aa7fc038fe4e12003
yunshan23612 <qian.yichi@northeastern.edu>
2025-02-27 13:43:13 -0500

    Update war.cs

--- Projects/War/war.cs
+++ Projects/War/war.cs
@@ -1,13 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-List<Card> deck = new List<Card>();//build a new list to store all the cards
-List<Card> discardPile = new();//a new list to store all the discarded cards
-Card playerHand = new();//player’s card
-Card dealerHand = new();//dealer’s card
+using System.Globalization;
 
+List<Card> deck = new List<Card>();// card pile
+List<Card> discardPile = new(); // Discard Pile
+Card playerHand = new(); // player hand
+Card dealerHand = new(); // computer hand
+int playerScore = 0; // player score
+int dealerScore = 0; // computer score
 
-try{
-        foreach (Suit suit in Enum.GetValues<Suit>())
+try
+{
+    // creat 52 cards 
+    foreach (Suit suit in Enum.GetValues<Suit>())
     {
         foreach (Value value in Enum.GetValues<Value>())
         {
@@ -17,91 +22,173 @@ try{
                 Value = value,
             });
         }
-    }//add 52 cards with 4 different patterns combined with 13 numbers
-    Shuffle(deck);
-    while(deck.Count > 0){
+    }
+
+    Shuffle(deck); // shuffle
+
+    while (true) // the game main loop
+    {
+        if (deck.Count == 0) // if the card pile is empty
+        {
+            Console.Clear();
+            Console.WriteLine("the card pile is empty！\n1. shuffle and continue\n2. end the game");
+            Console.Write("please choose: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1") // shuffle and continue
+            {
+                deck = new List<Card>(discardPile);
+                discardPile.Clear();
+                Shuffle(deck);
+            }
+            else
+            {
+                break;
+            }
+        }
+
         start:
         Console.Clear();
-        Console.WriteLine("Press Enter to draw a card(escape to quit):");
-        Console.WriteLine(deck.Count + "cards left in the deck");//render the hint
+        Console.WriteLine("press Enter to get a hand（Esc to escept）");
+        Console.WriteLine($"deck left: {deck.Count}");
+        Console.WriteLine($"current score - player: {playerScore}, computer: {dealerScore}");
+
         switch (Console.ReadKey(true).Key)
         {
             case ConsoleKey.Enter:
-                playerHand = deck[^1];//get the last variable in the list as player’s hand
-                deck.RemoveAt(deck.Count - 1);//remove it from the list
-                discardPile.Add(playerHand);//add it into the discard pile
-                Console.WriteLine("You draw " + playerHand.Suit + " " + playerHand.Value);//show the player’s hand
+                playerHand = deck[^1]; // player draw a card
+                deck.RemoveAt(deck.Count - 1);
+                discardPile.Add(playerHand); // add the card to discard pile
+
+                dealerHand = deck[^1]; // computer draw a card
+                deck.RemoveAt(deck.Count - 1);
+                discardPile.Add(dealerHand); // add the card to discard pile
                 break;
+
             case ConsoleKey.Escape:
                 return;
+
             default:
-                Console.WriteLine("Press Enter only.");
+                Console.WriteLine("press Enter to continue");
                 goto start;
         }
-        Console.WriteLine("Dealer Draw a card:");//do the same for dealer
-                dealerHand = deck[^1];
-                deck.RemoveAt(deck.Count - 1);
-                Console.WriteLine("Dealer draw " + dealerHand.Suit +" " + dealerHand.Value);
-//compare hands for both player
-        if(playerHand.Value > dealerHand.Value){
-            Console.WriteLine("You win");
+
+        Console.Clear();
+        Console.WriteLine("you draw:");
+        DisplayCard(playerHand);
+        Console.WriteLine("\n the computer draw:");
+        DisplayCard(dealerHand);
+
+        // 比较牌面大小
+        if (playerHand.Value > dealerHand.Value)
+        {
+            Console.WriteLine("\n you win！");
+            playerScore++;
         }
-        else if(playerHand.Value < dealerHand.Value){
-            Console.WriteLine("You lose");
+        else if (playerHand.Value < dealerHand.Value)
+        {
+            Console.WriteLine("\n you lose！");
+            dealerScore++;
         }
-        else{
-            Console.WriteLine("Draw");
+        else
+        {
+            Console.WriteLine("\n draw！");
         }
-        if(deck.Count == 0){//shuffle the card after the deck pile was run out
-            Shuffle(discardPile);
-            deck = discardPile;
-        }
-        Console.WriteLine("Press any key to Continue");
+
+        Console.WriteLine($"\n current score - player: {playerScore}, computer: {dealerScore}");
+        Console.WriteLine("\n press any key to continue...");
         Console.ReadKey();
     }
+}
+finally
+{
+    Console.WriteLine("\n the game end！");
+}
+
+// show card
+void DisplayCard(Card card)
+{
+    foreach (var line in card.Render())
+    {
+        Console.WriteLine(line);
+    }
+}
+
+// shuffle
 void Shuffle(List<Card> cards)
-{//method to shuffle the card
+{
+    Random rnd = new Random();
     for (int i = 0; i < cards.Count; i++)
     {
-        int swap = Random.Shared.Next(cards.Count);
+        int swap = rnd.Next(cards.Count);
         (cards[i], cards[swap]) = (cards[swap], cards[i]);
     }
 }
 
-
-}
-finally{
-    Console.WriteLine("Game End");
-}
-
-
-
-
-class Card {//a class of cards with 2 properties
+// class card
+class Card
+{
     public Suit Suit;
     public Value Value;
+
+    public string[] Render()
+    {
+        char suit = Suit switch
+        {
+            Suit.Hearts => '♥',
+            Suit.Clubs => '♣',
+            Suit.Spades => '♠',
+            Suit.Diamonds => '♦',
+            _ => ' '
+        };
+
+        string value = Value switch
+        {
+            Value.Ace => "A ",
+            Value.Ten => "10",
+            Value.Jack => "J ",
+            Value.Queen => "Q ",
+            Value.King => "K ",
+            _ => ((int)Value).ToString(CultureInfo.InvariantCulture) + " "
+        };
+
+        return new string[]
+        {
+            "┌───────┐",
+            $"│ {value}    │",
+            "│       │",
+            $"│   {suit}   │",
+            "│       │",
+            $"│    {value} │",
+            "└───────┘"
+        };
+    }
 }
-enum Suit{//the card’s suit
 
-
+// Enumeration - Suit
+enum Suit
+{
     Hearts,
     Clubs,
     Spades,
     Diamonds,
 }
+
+// Enumeration - value
 enum Value
-{//the card’s value
-    Ace   = 14,
-    Two   = 02,
-    Three = 03,
-    Four  = 04,
-    Five  = 05,
-    Six   = 06,
-    Seven = 07,
-    Eight = 08,
-    Nine  = 09,
-    Ten   = 10,
-    Jack  = 11,
+{
+    Ace = 14,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 11,
     Queen = 12,
-    King  = 13,
+    King = 13,
 }
+
